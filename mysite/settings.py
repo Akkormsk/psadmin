141ae860c53b74eb3c5ff233702d8a939934f8a3
akkormsk <akkormsk@gmail.com>
2026-01-06 15:54:50 +0100

    improve static

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = "/static/"
+STATIC_URL = "/assets/"
+WHITENOISE_STATIC_PREFIX = "/assets/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
