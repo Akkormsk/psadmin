50c4b8dcc84d7739a87e3bd5ee185d67a041926a
akkormsk <akkormsk@gmail.com>
2026-01-07 14:55:45 +0100

    начал работать с визуалом

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 WHITENOISE_USE_FINDERS = True
