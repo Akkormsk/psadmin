2903e62cb13a2946795e9ba86257fce75cf79897
akkormsk <akkormsk@gmail.com>
2026-01-24 16:08:30 +0100

    исправил ненужную кастомную админку админку

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from mysite.adminsite import admin_site  # <-- наш сайт админки
+from django.contrib import admin
+# from mysite.adminsite import admin_site  # <-- наш сайт админки
 
 urlpatterns = [
-    path("admin/", admin_site.urls),
+    # path("admin/", admin_site.urls),
+    path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("calculator/", include("calculator.urls")),
     path("cash/", include("cash.urls")),
