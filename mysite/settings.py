4787094bba9678a0455f8c4a953aacdc2e61efa9
akkormsk <akkormsk@gmail.com>
2026-04-12 09:49:28 +0400

    fix deploy settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,6 +148,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 WHITENOISE_USE_FINDERS = True
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
